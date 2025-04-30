051fc8dc320a3bc691e3d1b2bf12506d28853d9d
pfthomas <78826836+pfthomas@users.noreply.github.com>
2025-04-30 13:22:12 -0400

    Responsive design
    readjusted CSS, HTML, and JS to be media responsive for different screen sizes

--- js/script.js
+++ js/script.js
@@ -89,5 +89,14 @@ function restartQuiz() {
     document.getElementById("button").style.visibility = "visible";
 }
 
+//function to make the drop down hamburger on smaller screens function
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navbar = document.getElementById('navbar');
+
+    menuToggle.addEventListener('click', function () {
+        navbar.classList.toggle('active');
+    });
+});
 
 
